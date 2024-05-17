27d5ee577d42c73400f70047e95ccd201bf29909
shubham282raj <shubham282.me@gmail.com>
2024-05-18 00:13:33 +0530

    Puppeteer: Added puppeteer stealth plugin

--- backend/src/utils/browser.js
+++ backend/src/utils/browser.js
@@ -1,4 +1,7 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-extra";
+import stealth_plugins from "puppeteer-extra-plugin-stealth";
+
+puppeteer.use(stealth_plugins());
 
 export const startBrowser = async () => {
   try {
