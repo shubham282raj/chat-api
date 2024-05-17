1cf06ca92fade70c1dcad4d78387c412ecb9c0bd
Shubham Raj <shubham282raj@gmail.com>
2024-05-17 06:12:39 +0530

    Backend: Added chat-gpt search api

--- backend/src/routes/chat-gpt.js
+++ backend/src/routes/chat-gpt.js
@@ -1,5 +1,41 @@
 import { Router } from "express";
+import { search_handler } from "../middleware/search-handler.js";
+import { browser } from "../index.js";
+import { expect } from "playwright/test";
 
 const router = Router();
+
+router.get("/search", search_handler, async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+
+    const page = await browser.newPage();
+    await page.goto("http://www.chatgpt.com");
+    await page.locator("[id=prompt-textarea]").fill(prompt);
+    await page.press("[id=prompt-textarea]", "Enter");
+
+    await expect(
+      page.locator("[data-testid=conversation-turn-3]")
+    ).toBeVisible();
+    await expect(
+      page
+        .locator("[data-testid=conversation-turn-3]")
+        .locator(".text-token-text-tertiary")
+    ).toBeVisible({
+      timeout: 30000,
+    });
+
+    const text = await page
+      .locator("[data-testid=conversation-turn-3]")
+      .locator(".markdown")
+      .textContent();
+    await page.close();
+
+    return res.status(200).send({ message: text });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send({ message: "Something went wrong!" });
+  }
+});
 
 export default router;
